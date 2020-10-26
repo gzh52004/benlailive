960cc87ee6967c38c0bec0541503a8babdb9fac8
林韦良 <952185530@qq.com>
2020-10-26 21:02:18 +0800

    购物车部分样式

--- react_app/src/page/cart/index.js
+++ react_app/src/page/cart/index.js
@@ -1,9 +1,63 @@
+import request from '../../utils/request'
+import React,{useState,use} from 'react'
+import antdMB ,{ Icon, Checkbox } from 'antd-mobile'
+import {useHistory}  from 'react-router-dom'
 
-import React from 'react'
+import ProductCart from '../../component/cart/product_cart'
+
+import '../../assets/public/common.css'
+import './index.scss'
+// let findQuery = {
+//     _id:'5f953467bceb3b39304c7e48'
+// }
+
+
+//  request.get("/home/Gethomepage",{params:{
+//      findQuery
+//  }}).then(res => {
+//      console.log(res.data)
+//  })
+
+const CheckboxItem = Checkbox.CheckboxItem;
 
 function Cart() {
+    const [ischeck,changeCheck] = useState(false)
+    
     return (
-        <div className="cart">Cart</div>
+        <>
+       <header> 
+           <Icon type='left' size='lg'></Icon>
+           <div className="cart_title">
+               <h3>购物车</h3>
+               <p>配送至：上海市</p>
+           </div>
+       
+        <div className="edit">
+        <p>编辑</p>
+        <Icon type='ellipsis' size='md'></Icon>
+        </div>
+       </header>
+
+       <div className="white_space"></div>
+       <ProductCart></ProductCart>
+       <footer>
+       <div className="allcheck">
+       <Checkbox key={ischeck} checked={ischeck} onClick={()=> {
+           changeCheck(!ischeck);
+       }}>
+       </Checkbox>
+       <span>全选</span>
+       </div>
+       <div className="accounting">
+           <div className="allPrice">
+               合计:<span>￥0</span>
+           </div>
+           <a href="###">
+               去结算
+           </a>
+       </div>
+       </footer>
+       </>
     )
 }
 export default Cart
