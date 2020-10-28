53cb8c19f73c8dfa92ae39dfb86da90d6a2d91e8
unknown <1320089887@qq.com>
2020-10-28 09:02:40 +0800

    密码明、密文显示

--- react_app/src/page/login/index.jsx
+++ react_app/src/page/login/index.jsx
@@ -1,26 +1,79 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './login.scss'
 import {Button,Icon} from 'antd-mobile';
 import pic1 from './pic1.png'
 
 function Login() {
-    changeIcon={
-        
-    }
+        //点击眼睛明文/密文显示密码
+        const changeType = function(){
+            let aaa =  document.getElementsByClassName("ul1-li2-input")
+            let bbb = document.getElementsByClassName("ul1-li2-a1")
+            let ccc = document.getElementsByClassName("ul1-li2-a1-none")
+            if(aaa[0].type === "password"){
+                aaa[0].setAttribute("type","text")
+                bbb[0].style.display="none"
+                ccc[0].style.display="block"
+            }else{
+                aaa[0].setAttribute("type","password")
+                bbb[0].style.display="block"
+                ccc[0].style.display="none"
+            }
+        }
+
+        //登录按钮是否可以点击
+        const [user,changeuse] = useState({username:'',password:''})
+        const buttonState = function(){
+            if(user.username != "" && user.password !=""){
+                    let result = "ul1-li4-button"
+                    return result
+                }
+        }
+
+
     return (
         <div className="login">
             <div className="return"><Icon type="left" /></div>
             <div className="content">
-
             <img className="pic1" src={pic1} alt=""></img>   
             <ul className="ul1">
-                <li className="ul1-li1"><p className="icon-user"></p><p className="ul1-li1-p"><input type="text" className="ul1-li1-input" placeholder="请输入手机号/邮箱"></input></p></li>
+                <li className="ul1-li1">
+                    <p className="icon-user"></p>
+                    <p className="ul1-li1-p">
+                        <input type="text" value={user.username} onChange={(e) => {
+                    let value = {
+                        ...user,
+                        username:e.target.value
+                    }
+                    changeuse(value)
+                }}         
+                className="ul1-li1-input" 
+                placeholder="请输入手机号/邮箱">
+                    </input>
+                    </p>
+                    </li>
                 <li className="ul1-li2">
-                <p className="icon-suo"></p><p className="ul1-li2-p"><input type="password" className="ul1-li2-input" placeholder="请输入密码"></input></p>
-                <a href="" onClick={changeIcon} className="ul1-li2-a1"></a>
+                <p className="icon-suo"></p>
+                <p className="ul1-li2-p">
+                    <input type="password" value={user.password} onChange={
+                    (e)=>{
+                        let value={
+                            ...user,
+                            password:e.target.value
+                        }
+                        changeuse(value)
+                    }
+                } 
+                className="ul1-li2-input" placeholder="请输入密码">
+                    </input>
+                    </p>
+                <a className="ul1-li2-a1" style={{display:"block"}} onClick={changeType}></a>
+                <a className="ul1-li2-a1-none" style={{display:"none"}} onClick={changeType}></a>
                 <a className="ul1-li2-a2" href="">忘记密码</a>
                 </li>
-                <li><Button type="primary">登录</Button></li>
+                <li>
+            <p style={{display:"none"}} className="ul1-li3-p">yyy</p>
+                    <Button type="primary" className={buttonState()}>登录</Button>
+                </li>
                 <li className="ul1-li4"><a className="ul1-li4-a2">注册账号</a></li>
             </ul>
             </div>
--- react_app/src/page/reg/index.jsx
+++ react_app/src/page/reg/index.jsx
@@ -4,6 +4,23 @@ import {Button,Icon} from 'antd-mobile';
 import pic1 from './pic1.png'
 
 function Reg() {
+    const changeType = function(){
+        let aaa =  document.getElementsByClassName("ul1-li2-input")
+        let bbb = document.getElementsByClassName("ul1-li2-a1")
+        let ccc = document.getElementsByClassName("ul1-li2-a1-none")
+        console.log(aaa[0].type)
+        if(aaa[0].type === "password"){
+            aaa[0].setAttribute("type","text")
+            console.log(bbb[0].style.display)
+            bbb[0].style.display="none"
+            ccc[0].style.display="block"
+        }else{
+            aaa[0].setAttribute("type","password")
+            bbb[0].style.display="block"
+            ccc[0].style.display="none"
+        }
+        
+    }
     return (
         <div className="reg">
             <div className="return"><Icon type="left" /></div>
@@ -14,8 +31,8 @@ function Reg() {
                 <li className="ul1-li1"><p className="icon-user"></p><p className="ul1-li1-p"><input type="text" className="ul1-li1-input" placeholder="请输入手机号/邮箱"></input></p></li>
                 <li className="ul1-li2">
                 <p className="icon-suo"></p><p className="ul1-li2-p"><input type="password" className="ul1-li2-input" placeholder="请输入密码"></input></p>
-                <a href="" className="ul1-li2-a1"></a>
-                {/* <a className="ul1-li2-a2" href="">忘记密码</a> */}
+                <a className="ul1-li2-a1" style={{display:"block"}} onClick={changeType}></a>
+                <a className="ul1-li2-a1-none" style={{display:"none"}} onClick={changeType}></a>
                 </li>
                 <li><Button type="primary">注册</Button></li>
                 <li className="ul1-li4"><a className="ul1-li4-a2">登录账号</a></li>
