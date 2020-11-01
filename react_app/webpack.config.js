594d7d5873ecb0cf4cca2a9cfc43487adb9081c7
林韦良 <952185530@qq.com>
2020-11-01 13:06:55 +0800

    1

--- react_app/webpack.config.js
+++ react_app/webpack.config.js
@@ -12,9 +12,9 @@ module.exports = {
         //默认扩展名
         extensions: ['.js', '.jsx'],
         //路径别名
-        // alias: {
-        //     '@': path.resolve('./src')
-        // }
+        alias: {
+            '@': path.resolve('./src')
+        }
     },
     module: {
         rules: [
